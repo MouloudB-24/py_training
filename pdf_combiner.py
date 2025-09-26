bb78adfa89cfcb54a4f38b63c9ffb848f51041b4
Mouloud <mouloud.bellil@outlook.fr>
2025-09-26 08:16:10 +0200

    script extraction du texte depuis pdf

--- pdf_combiner.py
+++ pdf_combiner.py
@@ -1,5 +1,7 @@
+from pprint import pprint
 from PyPDF2 import PdfWriter, PdfReader
 
+# PDF - Combiner des fichiers .pdf
 
 # Créer un contenu pdf vide en mémoire
 contenu_sortie = PdfWriter()
@@ -19,4 +21,18 @@ with open("data/recap.pdf", "rb") as f:
 
 # Ecire le pdf combiné
 with open("data/pdf_combiner.pdf", "wb") as f:
-    contenu_sortie.write(f) 
+    contenu_sortie.write(f)
+    
+
+# PDF - Extraction du texte
+with open("data/recap.pdf", "rb") as f:
+    reader_pdf = PdfReader(f)
+    texte = ""
+    for num_page, contenu_page in enumerate(reader_pdf.pages, start=1):
+        contenu_texte = contenu_page.extract_text()
+        if contenu_texte: # Evite les pages vide
+            texte += f"\n------- Page {num_page} ------\n"
+            texte += contenu_texte.strip() + "\n"
+
+
+print(texte)
