ac29703b3682d8e9c68703b28e9e693fa690e056
Mouloud <mouloud.bellil@outlook.fr>
2025-10-03 09:04:54 +0200

    ajout docstring et affichage des résultats

--- traveling_salesman.py
+++ traveling_salesman.py
@@ -1,18 +1,12 @@
 # Problem : Traveling Salesman
 from math import sqrt
-import math
-from typing import List, Tuple
 
-
-def load_cities(file: str) -> List[Tuple]:
+def load_cities(file):
     """Load cities from a text file and return them as a list of tuples.
-
     Args:
         file (str): path that points to the test file
-
     Returns:
-        List[Tuple]: list of tuples containing city data
-           
+        List[(name, x, y)]: list of tuples containing city data
     >>> Example
     "paris 100, 220" -> [(paris, 100, 220)]
     """
@@ -31,13 +25,28 @@ def load_cities(file: str) -> List[Tuple]:
 
 
 def get_distance_between_two_cities(city1, city2):
-    return math.sqrt((city1[-2] - city2[-2])**2 + (city1[-1] - city2[-1])**2)
+    """Calcul la distance entre deux villes
+
+    Args:
+        city1 (tuple): _description_
+        city2 (tuple): _description_
+
+    Returns:
+        float: distance
+    """
+    return sqrt((city1[-2] - city2[-2])**2 + (city1[-1] - city2[-1])**2)
 
 
 def get_itineraire_greedy(cities):
+    """Get the optimal route from one city to the nearset unvisited city
+    Args:
+        cities (list): cities list
+    Returns:
+        list: list of cities oredered accoding to the optima route
+    """
     itineraire = []
     i = 1
-    while len(cities) > 1:
+    while not len(cities) == 1:
         visited_city = cities.pop(0)
         itineraire.append(visited_city)
         distance_min = float("inf")
@@ -54,26 +63,33 @@ def get_itineraire_greedy(cities):
 
 
 def get_distance_total(itineraire):
+    """Calcute the distance of the optimal route
+    Args:
+        itineraire (list): optimal route
+    Returns:
+        float: distance
+    """
     distance_tot = 0
     for i in range(len(itineraire)-1):
         distance_tot += get_distance_between_two_cities(itineraire[i], itineraire[i+1])
     return distance_tot
 
 
+# Show results
 cities = load_cities("data/villes.txt")
 print(f"The number of cities is:  {len(cities)}\n")
 
 paris_bordeaux = get_distance_between_two_cities(cities[0], cities[1])
-print(f"Distance entre Paris et Bordeaux est : {paris_bordeaux}")
+print(f"Distance between Paris and Bordeaux is: {paris_bordeaux}")
 paris_bordeaux = get_distance_between_two_cities(cities[1], cities[2])
-print(f"Distance entre Bordeaux et Marseille est : {paris_bordeaux}")
+print(f"Distance between Bordeaux and Marseille is: {paris_bordeaux}")
 
 # Calcul l'itineraire la plus court
 itineraire = get_itineraire_greedy(cities)
-print(itineraire[:10])
+print(f"\nThe first 10 cities of the optimal route: \n{[city[0] for city in itineraire[:10]]}")
 
 # Distance totale :
 distance_tot = get_distance_total(itineraire)
-print(f"\nLa distance totale de l'itineraire est : {distance_tot}")
+print(f"\nTotal distance of the optima; route: {distance_tot}")
 
 
